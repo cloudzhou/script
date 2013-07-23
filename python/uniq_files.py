f1cf42b7e8cd08b78e126507a825f3b9ff102d01
cloudzhou <azhoulinux@gmail.com>
2013-07-23 15:33:13 +0800

    only for file isfile

--- python/uniq_files.py
+++ python/uniq_files.py
@@ -6,13 +6,16 @@ import os, sys, codecs
 basepath = '/tmp/ss'
 uniq_line = {}
 for filepath in os.listdir(basepath):
+    abs_filepath = basepath + '/' + filepath
+    if not os.path.isfile(abs_filepath):
+        continue
     filtered_line = []
-    with codecs.open(basepath + '/' + filepath, encoding='utf-8') as readF:
+    with codecs.open(abs_filepath, encoding='utf-8') as readF:
         for line in readF:
             if line in uniq_line:
                 continue
             uniq_line[line] = 1
             filtered_line.append(line)
-    with codecs.open(basepath + '/' + filepath, encoding='utf-8', mode='w+') as writeF:
+    with codecs.open(abs_filepath, encoding='utf-8', mode='w+') as writeF:
         writeF.write(''.join(filtered_line))
         writeF.flush()
