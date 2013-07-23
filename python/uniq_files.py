7c84f71f2e22c4f2261cd8d58656d8ac1a94ebde
cloudzhou <azhoulinux@gmail.com>
2013-07-23 15:22:50 +0800

    add uniq files python

---
+++ python/uniq_files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os, sys, codecs
+
+basepath = '/tmp/ss'
+uniq_line = {}
+for filepath in os.listdir(basepath):
+    filtered_line = []
+    with codecs.open(basepath + '/' + filepath, encoding='utf-8') as readF:
+        for line in readF:
+            if line in uniq_line:
+                continue
+            uniq_line[line] = 1
+            filtered_line.append(line)
+    with codecs.open(basepath + '/' + filepath, encoding='utf-8', mode='w+') as writeF:
+        writeF.write(''.join(filtered_line))
+        writeF.flush()
